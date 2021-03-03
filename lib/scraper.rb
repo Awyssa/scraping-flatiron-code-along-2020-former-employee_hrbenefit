aaa76b23b0d2cfa894eac42e8b0828f62560583b
Michael Ernest <michael.ehson@gmail.com>
2021-03-03 13:27:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,15 +9,10 @@ class Scraper
   def get_page
 
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #doc.css(".post").each do |post|
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
-    #end
-  end
-end
 
-def get_courses
-  self.get_page.css(".post")
+    def get_courses
+      self.get_page.css(".post")
+    end
+    
+  end
 end
