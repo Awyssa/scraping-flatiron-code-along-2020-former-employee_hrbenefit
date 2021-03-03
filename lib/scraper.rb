a1085d14216d4d666f7bdac1003a5e712b5879b3
Michael Ernest <michael.ehson@gmail.com>
2021-03-03 13:20:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
 
     def get_courses
       self.get_page.css(".post")
-
+    end
     #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
